d00d389c5d03801775646a38639fd5e65ff4eec3
gvimra <ana.gvimradze@yahoo.com>
2017-10-12 11:45:41 +0200

    TD2 Papier-Rock-Scissors
    v2

--- TD2Jeux/src/fr/p10/miage/rps/model/RockPaperScissors.java
+++ TD2Jeux/src/fr/p10/miage/rps/model/RockPaperScissors.java
@@ -2,7 +2,18 @@ package fr.p10.miage.rps.model;
 
 public class RockPaperScissors {
 
-    public RPS.Result play(RPS.RPSEnum p1, RPS.RPSEnum p2){
+    public RPS.Result play(RPS.RPSEnum p1, RPS.RPSEnum p2) {
+
+        if (p1.equals(p2)) {
+            return RPS.Result.TIE;
+        }
+        else if ((p1.equals(RPS.RPSEnum.ROCK) && p2.equals(RPS.RPSEnum.SCISSORS)) ||
+                (p1.equals(RPS.RPSEnum.SCISSORS) && p2.equals(RPS.RPSEnum.PAPER)) ||
+                (p1.equals(RPS.RPSEnum.PAPER) && p2.equals(RPS.RPSEnum.ROCK))){
+            return RPS.Result.WIN;
+        }
+        else
         return RPS.Result.LOST;
     }
+
 }
--- TD2Jeux/test/fr/p10/miage/rps/model/RockPaperScissorsTest.java
+++ TD2Jeux/test/fr/p10/miage/rps/model/RockPaperScissorsTest.java
@@ -1,6 +1,8 @@
 package fr.p10.miage.rps.model;
 
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import static org.testng.Assert.*;
@@ -10,16 +12,79 @@ public class RockPaperScissorsTest {
     private RockPaperScissors rps;
 
     @BeforeClass
-    public void initial(){
+    public void setUP(){
+
         rps = new RockPaperScissors();
     }
 
-    @Test
-    public void testPlay() throws Exception {
-
-        assertEquals(rps.play(RPS.RPSEnum.ROCK, RPS.RPSEnum.SCISSORS), RPS.Result.LOST);
-
-
+    public void tearDown(){
+        rps = null;
     }
+
+    @Parameters({"paper", "rock"})
+    @Test
+    public void testWinPlay(String p1, String p2) throws Exception {
+
+        assertEquals(rps.play(RPS.RPSEnum.valueOf(p1), RPS.RPSEnum.valueOf(p2)), RPS.Result.WIN);
+    }
+
+    @Parameters({"paper", "paper"})
+    @Test
+    public void testTiePlay(String p1, String p2) throws Exception {
+
+        assertEquals(rps.play(RPS.RPSEnum.valueOf(p1), RPS.RPSEnum.valueOf(p2)), RPS.Result.TIE);
+    }
+
+    @Parameters({"rock", "paper"})
+    @Test
+    public void testLostPlay(String p1, String p2) throws Exception {
+
+        assertEquals(rps.play(RPS.RPSEnum.valueOf(p1), RPS.RPSEnum.valueOf(p2)), RPS.Result.LOST);
+    }
+
+
+    @DataProvider (name = "winData")
+        public Object[][] createWinData(){
+         return new Object[][]{
+                 {RPS.RPSEnum.ROCK , RPS.RPSEnum.SCISSORS},
+                 {RPS.RPSEnum.SCISSORS , RPS.RPSEnum.PAPER},
+                 {RPS.RPSEnum.PAPER , RPS.RPSEnum.ROCK},
+         };
+    }
+
+    @Test (dataProvider = "winData")
+    void testWinPlay (RPS.RPSEnum p1 , RPS.RPSEnum p2){
+        assertEquals(rps.play(p1, p2), RPS.Result.WIN);
+    }
+
+    @DataProvider (name = "tieData")
+    public Object[][] createTieData(){
+        return new Object[][]{
+                {RPS.RPSEnum.SCISSORS , RPS.RPSEnum.SCISSORS},
+                {RPS.RPSEnum.PAPER , RPS.RPSEnum.PAPER},
+                {RPS.RPSEnum.ROCK , RPS.RPSEnum.ROCK},
+        };
+    }
+
+    @Test (dataProvider = "tieData")
+    void testTiePlay (RPS.RPSEnum p1 , RPS.RPSEnum p2){
+        assertEquals(rps.play(p1, p2), RPS.Result.TIE);
+    }
+
+    @DataProvider (name = "lostData")
+    public Object[][] createLostData(){
+        return new Object[][]{
+                {RPS.RPSEnum.ROCK , RPS.RPSEnum.PAPER},
+                {RPS.RPSEnum.SCISSORS , RPS.RPSEnum.ROCK},
+                {RPS.RPSEnum.PAPER , RPS.RPSEnum.SCISSORS},
+        };
+    }
+
+    @Test (dataProvider = "lostData")
+    void testLostPlay (RPS.RPSEnum p1 , RPS.RPSEnum p2){
+        assertEquals(rps.play(p1, p2), RPS.Result.LOST);
+    }
+
+
 
 }
